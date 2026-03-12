5107f30b4ca275c6006598e2bc80842b802454d4
tulsee-agrawal <tulsee.bargarh@gmail.com>
2026-03-12 16:25:10 +0530

    Coded UC18 - Linear Search for Bogie ID

--- TrainApp/src/com/trainapp/TrainApp.java
+++ TrainApp/src/com/trainapp/TrainApp.java
@@ -1,42 +1,56 @@
 package com.trainapp;
 
-import java.util.Arrays;
-
 /**
- * MAIN CLASS - UC17TrainApp
- * 
- * Use Case 17: Sort Bogie Names Using Arrays.sort()
+ * MAIN CLASS - UC18TrainApp
+ *
+ * Use Case 18: Linear Search for Bogie ID
  *
  * Description:
- * This class demonstrates sorting of bogie type names
- * alphabetically using Java's built-in Arrays.sort() method.
+ * This class demonstrates searching for a specific bogie ID
+ * using a simple Linear Search algorithm.
  *
  * At this stage, the application:
- * - Creates an array of bogie names
- * - Uses Arrays.sort() for sorting
- * - Displays sorted results
+ * - Creates an array of bogie IDs
+ * - Accepts a search key
+ * - Traverses array sequentially
+ * - Stops when match is found
+ * - Displays search result
  *
- * This maps optimized sorting using Java library utilities.
+ * This maps basic searching logic using sequential traversal.
  *
  * @author TulseeAgrawal
- * @version 17.0
+ * @version 18.0
  */
 public class TrainApp {
 
     public static void main(String[] args) {
-        System.out.println(" UC17 - Sort Bogie Names Using Arrays.sort()");
+        System.out.println("======UC18 - Linear Search for Bogie ID======");
+        
+        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
-        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
+        String searchId = "BG309";
 
-        System.out.println("\nOriginal Bogie Names:");
-        System.out.println(Arrays.toString(bogieNames));
+        System.out.println("Available Bogie IDs:");
+        for (String id : bogieIds) {
+            System.out.println(id);
+        }
+        System.out.println();
 
-        // Sort alphabetically
-        Arrays.sort(bogieNames);
+      
+        boolean found = false;
+        for (String id : bogieIds) {
+            if (id.equals(searchId)) {
+                found = true;
+                break; 
+            }
+        }
 
-        System.out.println("\nSorted Bogie Names (Alphabetical):");
-        System.out.println(Arrays.toString(bogieNames));
+        if (found) {
+            System.out.println("Bogie " + searchId + " found in train consist.");
+        } else {
+            System.out.println("Bogie " + searchId + " not found in train consist.");
+        }
 
-        System.out.println("\nUC17 sorting completed...");
+        System.out.println("\nUC18 search completed...");
     }
 }
